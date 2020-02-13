551709bd7249c24c945f4ed741b2fc8532ede387
涂峰旗 <185441127@qq.com>
2020-02-13 16:03:14 +0800

    feat:ip access support

--- app/Classes/Utils/StringUtil.php
+++ app/Classes/Utils/StringUtil.php
@@ -8,6 +8,6 @@ class StringUtil
     public static function routeReplaceForTag($route_str)
     {
         $replace = time();
-        return preg_replace('/:[0-9a-zA-Z_]+/', $replace, $route_str);
+        return preg_replace('/:[a-zA-Z]+[0-9a-zA-Z_]+/', $replace, $route_str);
     }
 }
